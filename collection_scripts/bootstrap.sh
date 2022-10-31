4abfc76aabcb408f445b64a8564a083e48cacb6b
Jay Schauer <jschauer@umich.edu>
2022-10-31 13:22:38 -0400

    Cleanup directory by moving firefox stuff

--- collection_scripts/bootstrap.sh
+++ collection_scripts/bootstrap.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
 sudo apt-get update
-sudo apt-get install -y python3-pip xvfb libnss3-dev firefox google-chrome-stable
+sudo apt-get install -y python3-pip xvfb libnss3-dev google-chrome-stable
 
 sudo pip install selenium
 
 sudo cp /vagrant/dnsproxy-linux-amd64-v0.46.2 /usr/local/bin/dnsproxy
-sudo cp /vagrant/geckodriver-v0.32.0-linux64 /usr/local/bin/geckodriver
 sudo cp /vagrant/chromedriver/ /usr/local/bin/chromedriver
